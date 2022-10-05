7c16539387ec4e045e19b632f17fa32123780423
rubensanzrosa <@Penesaurio12>
2022-10-05 21:04:07 +0200

    Sesion 11 - CRUD

--- src/Controller/Api/CatergoriaController.php
+++ src/Controller/Api/CatergoriaController.php
@@ -5,12 +5,14 @@ namespace App\Controller\Api;
 use App\Entity\Categoria;
 use App\Form\CategoriaType;
 use App\Repository\CategoriaRepository;
+use App\Repository\RestauranteRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use mysql_xdevapi\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\Json;
 
 
 /**
@@ -18,6 +20,8 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class CatergoriaController extends AbstractFOSRestController
 {
+    // CRUD
+    // Create , update, read , delete
 
     private $categoriaRepository;
 
@@ -35,27 +39,30 @@ class CatergoriaController extends AbstractFOSRestController
         return $this->categoriaRepository->findAll();
     }
 
+    // Traer una categoria
+    /**
+     * @Rest\Get (path="/{id}")
+     * @Rest\View(serializerGroups={"get_categoria"}, serializerEnableMaxDepthChecks= true)
+     */
+    public function getCategoria(Request $request){
+        $idCategoria = $request->get('id');
+        $categoria = $this->categoriaRepository->find($idCategoria);
+        if(!$categoria){
+            return new JsonResponse('No se ha encontrado categoria', Response::HTTP_NOT_FOUND);
+        }
+        return $categoria;
 
-//    public function createCategoria(Request $request){
-//        $categoria = $request->get('categoria');
-//
-//        if(!$categoria){//Comprueba si me llega algun dato pero no el tipo
-//            return new JsonResponse('Error en la peticion', Response::HTTP_BAD_REQUEST);
-//        }
-//        //Crear el objeto y hacer un set del nombre de la categoria que he recibido
-//        $cat = new Categoria();
-//        $cat->setCategoria($categoria);
-//        // Guardamos en base de datos
-//        $this->categoriaRepository->add($cat, true);
-//        //Enviar una respuesta al cliente
-//        return $cat;
-//    }
+    }
+
+
+
     /**
      * @Rest\Post (path="/")
      * @Rest\View (serializerGroups={"post_categoria"}, serializerEnableMaxDepthChecks= true)
      */
     public function createCategoria(Request $request){
-        // Formularios
+        // Formularios -> Es para manejas las peticiones, y validar tipado-> Null, si viene el texto en blanco ...etc
+        // Validator -> Null, ->> Maximo 100 de tamaño
         // 1. Creo el objeto vacio
         $cat = new Categoria();
         // 2. Inicializamos el form
@@ -67,10 +74,52 @@ class CatergoriaController extends AbstractFOSRestController
             return $form;
         }
         //5. Todo ok, guardo en BD
-        $categoria = $form->getData();
-        print_r($categoria);
+
+        $this->categoriaRepository->add($cat, true);
+        return $cat;
+    }
+
+    // Update con patch
+    /**
+     * @Rest\Patch(path="/{id}")
+     * @Rest\View (serializerGroups={"up_categoria"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function updateCategoria(Request $request){
+        //Me guardo el id de la categoria
+        $categoriaId = $request->get('id');
+
+        // OJO Comprobar que existe esa categoria
+        $categoria = $this->categoriaRepository->find($categoriaId);
+        if(!$categoria){
+            return new JsonResponse('No se ha encontrado', Response::HTTP_NOT_FOUND);
+        }
+        $form = $this->createForm(CategoriaType::class,$categoria,['method'=>$request->getMethod()]);
+        $form->handleRequest($request);
+        //Tenemos que comprobar la validez del form
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return new JsonResponse('Bad data', 400);
+        }
         $this->categoriaRepository->add($categoria, true);
         return $categoria;
+
+
+    }
+
+    //Delete
+    /**
+     * @Rest\Delete (path="/{id}")
+     *
+     */
+    public function deleteCategoria(Request $request){
+        $categoriaId = $request->get('id');
+        $categoria = $this->categoriaRepository->find($categoriaId);
+        if(!$categoria){
+           return new JsonResponse('No se ha encontrado', 400);
+        }
+
+        $this->categoriaRepository->remove($categoria, true);
+        return new JsonResponse('Categoria borrada', 200);
+
     }
 
 }
